cde6c25b638fadf9efcc2e03acb63aa5ffd66b3b
Hugo <78080239+mordeur@users.noreply.github.com>
2022-02-07 16:03:21 +0100

    nice

---
+++ resources/views/Parking/compte/Inscription.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <title>Début</title>
+      <nav>
+         <ul>
+            <li><a href="">Accueil</a></li>
+            <li><a href="">
+            Gestion d'équipes</a></li>
+            <li><a href="">
+            Gestion établissements</a></li>
+            <li><a href="">
+            Attributions chambres</a></li>
+         </ul>
+      </nav>
+</head>
+<body>
+
+</body>
+</html>
