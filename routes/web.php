caea1fd1a67258664bf7e2ab684a413c65c9b02e
Adelinejcqt <adeline.jcqt@outlook.fr>
2022-02-14 17:37:47 +0100

    Merge branch 'main' of https://github.com/mordeur/Parking-It-rations-2

--- routes/web.php
+++ routes/web.php
@@ -1,52 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Controleur_site;
 
-//Page de test et routes de test pour tous les collaborateurs
-//Ces dernières peuvent tester leur routes sur leurs pages dédiées et tester les routes ici avant de les mettre definitevement dans le reste des routes
-
-//test Valentin
-Route::get('/valentin', function () {
-    return view('PageTestValentin');
-});
-
-//test Adeline
-Route::get('/adeline', function () {
-    return view('PageTestAdeline');
-});
-
-//test Hugo
-Route::get('/hugo', function () {
-    return view('PageTestHugo');
-});
-
-////////////////////////////////////////////////////////////////////////
-
-//Exemple
-
-//Formulaire inscription exemple
-Route::get('/efi', 'InscriptionController@formulaire');
-
-//Création utilisateur exemple
-//Avec récupération des données du formulaire ci dessus
-Route::post('/exemple_formulaire_inscription', 'InscriptionController@traitement');
-
-//Liste utilisateurs exemple
-//exemple_liste_users.blade.php --> Page test pour afficher la liste des utilisateurs
-Route::get('/elu', 'UtilisateursController@listetest');
-
-//Connexion exemple
-Route::get('/connexion', 'ConnexionController@formulaire');
-Route::post('/connexion', 'ConnexionController@traitement');
-
-/* 
-Valentin : Pas compris à quoi ça sert donc pour l'instant je le mets en commentaire jusqu'à une explication
-Route::get('/',[Controleur_site::class,'accueil'])->name('accueil');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
 */
 
-//Page de démarrage
 Route::get('/', function () {
-    return view('compte/connection');
+    return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
