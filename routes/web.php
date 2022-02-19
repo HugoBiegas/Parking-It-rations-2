1cdf5cef360d8811df404335d46c1b29cc939308
Pito <90179414+vpoveda-amaral@users.noreply.github.com>
2022-02-19 12:19:52 +0100

    recupération des anciennes routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,48 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controleur_site;
 
+//Page de test et routes de test pour tous les collaborateurs
+//Ces dernières peuvent tester leur routes sur leurs pages dédiées et tester les routes ici avant de les mettre definitevement dans le reste des routes
+
+//test Valentin
+Route::get('/valentin', function () {
+    return view('PageTestValentin');
+});
+
+//test Adeline
+Route::get('/adeline', function () {
+    return view('PageTestAdeline');
+});
+
+//test Hugo
+Route::get('/hugo', function () {
+    return view('PageTestHugo');
+});
+
+////////////////////////////////////////////////////////////////////////
+
+//Exemple
+
+//Formulaire inscription exemple
+Route::get('/efi', 'InscriptionController@formulaire');
+
+//Création utilisateur exemple
+//Avec récupération des données du formulaire ci dessus
+Route::post('/exemple_formulaire_inscription', 'InscriptionController@traitement');
+
+//Liste utilisateurs exemple
+//exemple_liste_users.blade.php --> Page test pour afficher la liste des utilisateurs
+Route::get('/elu', 'UtilisateursController@listetest');
+
+//Connexion exemple
+Route::get('/connexion', 'ConnexionController@formulaire');
+Route::post('/connexion', 'ConnexionController@traitement');
+
+/* 
+Valentin : Pas compris à quoi ça sert donc pour l'instant je le mets en commentaire jusqu'à une explication
+Route::get('/',[Controleur_site::class,'accueil'])->name('accueil');
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,8 +54,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Page de démarrage
 Route::get('/', function () {
-    return view('welcome');
+    return view('compte/connection');
 });
 
 Route::get('/dashboard', function () {
